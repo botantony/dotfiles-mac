47604bab797820bca0231f9e96e1e16cbc4ebde7
botantony <antonsm21@gmail.com>
2024-12-27 00:47:29 +0100

    Ghostty config

--- .config/nvim/lua/plugins/presence.lua
+++ .config/nvim/lua/plugins/presence.lua
@@ -4,7 +4,7 @@ return {
 		-- The setup config table shows all available config options with their default values:
 		require("presence").setup({
 			-- General options
-			auto_update = false, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
+			auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
 			neovim_image_text = 'The only reason I use Neovim is because I can say "I use Neovim btw"', -- Text displayed when hovered over the Neovim image
 			main_image = "file", -- Main image display (either "neovim" or "file")
 			-- client_id           = "793271441293967371",       -- Use your own Discord application client id (not recommended)
